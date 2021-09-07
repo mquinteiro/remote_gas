4267e38ebff3ece57797d86950dbb67623d4ceff
M. Quinteiro @home <mquinteiro@gmail.com>
2021-09-07 18:56:55 +0200

    [FEAT] Pub/Sub subscriber side with dump of sql from gce, NOTE: add cic_host, cic_pass, cic_user, cic_database to cep_credentials.py

--- download_file.py
+++ download_file.py
@@ -1,17 +1,33 @@
 from google.cloud import storage
+from cep_credentials import gce_cert_json_name
 from sys import argv
 
-if len(argv) <2:
-    print("upload_file.py filename")
-    exit(0)
-
-print(argv[0])
-file_name = argv[1]
-storage_client = storage.Client.from_service_account_json('mquinteiro-8ef43ab73aee.json')
 bucket_name = 'cepsa_shares'
 
+def dowload_strings(bucket_name, blob_name):
+    storage_client = storage.Client.from_service_account_json(gce_cert_json_name)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    if blob.exists():
+        return blob.download_as_bytes()
+        
+    return None
 
-bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(file_name)
-blob.download_to_filename(file_name)
+def download_file(bucket_name, blob_name):
+    storage_client = storage.Client.from_service_account_json(gce_cert_json_name)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    blob.download_to_filename(file_name)
+
+if __name__ == '__main__':    
+    if len(argv) <2:
+        print("upload_file.py filename")
+        exit(0)
+    print(argv[0])
+    file_name = argv[1]
+
+
+
+
+
 #blob.upload_from_filename(file_name)
