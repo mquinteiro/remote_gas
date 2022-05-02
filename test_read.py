72e540d355b0da04bd2f95c0fb2660a5cbfd88b8
M. Quinteiro @home <mquinteiro@gmail.com>
2022-05-02 22:37:17 +0200

    [FIX] text codification y remote sinc queryes

--- test_read.py
+++ test_read.py
@@ -3,7 +3,7 @@ from cep_credentials import gce_cert_json_name, cic_host, cic_user, cic_pass, ci
 import re
 from download_file import dowload_strings, bucket_name, move_file
 
-file_name = "processed/20220429190006_cepsa_update.txt"
+file_name = "processed/20220429193006_cepsa_update.txt"
 strings = dowload_strings(bucket_name, file_name).decode('ISO-8859-1')
 s = strings.replace('delete from Terminales;\r\r\n','delete from Terminales@ENDCOMAND@')
 s2 = re.sub('\);\r+\n*',')@ENDCOMAND@',s)
@@ -11,7 +11,7 @@ queries = s2.split('@ENDCOMAND@')
 for my_strings in queries:
     if my_strings.find("Centro Paroquial e Social") != -1:
         print(my_strings)
-        db = mdb.connect(host=cic_host, user=cic_user, passwd=cic_pass, db=cic_database)
+        db = mdb.connect(host=cic_host, user=cic_user, passwd=cic_pass, db=cic_database, charset='utf8')
         cur = db.cursor()
         cur.execute(my_strings)
         db.commit()
